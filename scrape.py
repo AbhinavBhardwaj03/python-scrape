ea82ee50c0a4f966a69b3c600320593ca7aa92ed
Ankit Pandey <ankitechzz@gmail.com>
2023-04-04 17:45:19 +0530

    item name and price fetched

--- scrape.py
+++ scrape.py
@@ -5,24 +5,40 @@ import pandas as pd
 # define the URL to scrape
 url = 'https://swiggy.com/restaurants/hungry-hope-rbi-flats-colony-kankarbagh-patna-152335'
 
-# make a request to the URL
-response = requests.get(url)
+# make a request to the URL and handle connection errors
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+except requests.exceptions.RequestException as e:
+    print(f"Error: {e}")
+    exit()
 
-# parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.content, 'html.parser')
+# parse the HTML content using BeautifulSoup and handle parsing errors
+try:
+    soup = BeautifulSoup(response.content, 'html.parser')
+except Exception as e:
+    print(f"Error: {e}")
+    exit()
 
 # create empty lists to store the data
 item_names = []
 rupees = []
-# image_srcs = []
 
-# find all elements with the class 'styles_itemNameText__3ZmZZ'
-for item_name in soup.find_all(class_='styles_itemNameText__3ZmZZ'):
-    item_names.append(item_name.text.strip())
+# find all elements with the class 'styles_itemNameText__3ZmZZ' and handle attribute errors
+try:
+    for item_name in soup.find_all(class_='styles_itemNameText__3ZmZZ'):
+        item_names.append(item_name.text.strip())
+except AttributeError as e:
+    print(f"Error: {e}")
+    exit()
 
-# find all elements with the class 'ruppee'
-for rupee in soup.find_all(class_='rupee'):
-    rupees.append(rupee.text.strip())
+# find all elements with the class 'rupee' and handle attribute errors
+try:
+    for rupee in soup.find_all(class_='rupee'):
+        rupees.append(rupee.text.strip())
+except AttributeError as e:
+    print(f"Error: {e}")
+    exit()
 
 # create a pandas dataframe from the data
 data = {
